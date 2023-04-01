54aa1bb071db20bc5f480a14588afb342d3ad4ed
lerraytss <berdanali002@gmail.com>
2023-04-01 15:57:04 +0300

    anasayfa butonu eklendi

--- phpPages/proteinihtiyaci.php
+++ phpPages/proteinihtiyaci.php
@@ -9,88 +9,64 @@
   <link rel="stylesheet" href="../stylePages/proteinihtiyacı.css">
 </head>
 <body>
-</head>
-<body>
-  <nav>
-  <a href="../secim.html" style="position: absolute; top: 10px; right: 10px; padding: 30px 20px;  color:green; text-decoration: none;">Ana Sayfa</a>
-    <div class="logo">
-      <p><img src="../images/attachment_124010263-removebg-preview (1).png" alt=""></p>
-    </div>
-  </nav>
-
-  <header >
-
-    <div class="sema">
-    <header class="mt-3 text-center row">
-    <div class="col-sm-10 align-right">
-      <h4>Günlük Proteiasdadn Hesaplama</h4>
+<nav>
+      <a
+        href="../home.html"
+        style="position: absolute; top: 10px; right: 10px; padding: 30px 20px; color: green; text-decoration: none;"
+        >Ana Sayfa</a
+      >
+      <div class="logo">
+        <p>
+          <img
+            src="../images/attachment_124010263-removebg-preview (1).png"
+            alt=""
+          />
+        </p>
       </div>
-  </header>
-    </div>
-  
-  <div class="container mt-3">
-    <form method="POST">
-      
-       <div class="kutu1 ">
-     
-          <input type="number" class="form-control" id="kilo" name="kilo" style="margin-top: 6rem;" placeholder="Kilonuzu kilogram olarak girin.">
-       
-      
-          <input class="form-control" id="boy" name="boy" type="number" style="margin-top: 12rem;" placeholder="Boyunuzu santimetre olarak girin.">
-      
-          <input class="form-control" id="sex" name="sex" type="text" style="margin-top: 18rem;" placeholder="Cinsiyetinizi girin.">
+    </nav>
+  <div style="display:flex; justify-content: center; align-items:center;margin-top:50px">
+  <div style="display:flex;flex-direction:column; justify-content: center; align-items:center;width:500px; background:rgba(39, 169, 6, 0.1);padding:20px;border-radius: 50px;box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.1);" >
+    <h4>Günlük Protein Hesaplama</h4>
+    <div style="display:flex; flex-direction:column; gap:50px;" class="kutu1">
+          <input type="number" class="form-control" id="kilo" name="kilo" style="margin-top:50px; width: 400px;height:50px" placeholder="Kilonuzu kilogram olarak girin.">
+          <input class="form-control" id="boy" name="boy" type="number" style="height:50px" placeholder="Boyunuzu santimetre olarak girin.">
+          <input class="form-control" id="sex" name="sex" type="text" style="margin-bottom:50px;height:50px" placeholder="Cinsiyetinizi girin.">
         </div>
-      </div>
-      <div class="form-group mt-3 row">
-        <div class="col-sm-2"></div>
-        <div class="col-sm-10 align-right">
-          <button
-          > <h5>Hesapla</h5>
-        </button>
-        </div>
-      </div>
-    </form>
-  </header>
-    <div class="row"></div>
-      <div class="col-sm-2"></div>
-      <div class="col-sm-10 align-right">
-<div class=" sonuc">
+        <button style="margin-top:50px"><h5 style="padding:10px;width:250px;">Hesapla</h5></button>
+  </div>
+  </div>
       <?php
-function calculate($boy, $kilo ,$sex)
+function calculate($boy, $kilo, $sex)
 {
-  /*
-Yağsız Vücut Ağırlığı (ERKEK): = (0.73 * Boy[cm]) – 59.42
-Yağsız Vücut Ağırlığı (KADIN):= (0.65 * Boy[cm]) – 50.74
- yağsız vücut ağırlığının 2,75 katı
-  */
-  $erkekyagsizagirlik= (0.73 * $boy) -59.42;
-  $kadinyagsizagirlik= (0.65 * $boy) -50.74;
-  if ($_POST['sex'] == "erkek"){
-    echo "Günlük Protein İhtiyacınız : ".round($erkekyagsizagirlik *1.50,1). " gr ile " . round($erkekyagsizagirlik*2.50,1)." gr arasında";
+    /*
+    Yağsız Vücut Ağırlığı (ERKEK): = (0.73 * Boy[cm]) – 59.42
+    Yağsız Vücut Ağırlığı (KADIN):= (0.65 * Boy[cm]) – 50.74
+    yağsız vücut ağırlığının 2,75 katı
+     */
+    $erkekyagsizagirlik = (0.73 * $boy) - 59.42;
+    $kadinyagsizagirlik = (0.65 * $boy) - 50.74;
+    if ($_POST['sex'] == "erkek") {
+        echo "Günlük Protein İhtiyacınız : " . round($erkekyagsizagirlik * 1.50, 1) . " gr ile " . round($erkekyagsizagirlik * 2.50, 1) . " gr arasında";
 
-  }
-  elseif ($_POST['sex'] == "kadın"){
-    echo  "Günlük Protein İhtiyacınız : ".$kadinyagsizagirlik*2.50 . " gr ile " . $kadinyagsizagirlik*1.50." gr arasında";
-  }
-  else{
-    echo "Lütfen Cinsiyeti Kadın veya Erkek Olarak Giriniz";
-  }
-  $boy = filter_var(htmlentities(floatval($_POST['boy'])), FILTER_SANITIZE_NUMBER_FLOAT);
-  $kilo = filter_var(htmlentities(floatval($_POST['kilo'])), FILTER_SANITIZE_NUMBER_FLOAT);
-  $sex = isset($_POST['sex']) ? $_POST['sex'] : '';
-  
-  if ($boy && $kilo && $sex) {
-    calculate($boy, $kilo, $sex);
-  } else {
-    echo "Lütfen tüm alanları doldurun.";
-  }
-  
- }
+    } elseif ($_POST['sex'] == "kadın") {
+        echo "Günlük Protein İhtiyacınız : " . $kadinyagsizagirlik * 2.50 . " gr ile " . $kadinyagsizagirlik * 1.50 . " gr arasında";
+    } else {
+        echo "Lütfen Cinsiyeti Kadın veya Erkek Olarak Giriniz";
+    }
+    $boy = filter_var(htmlentities(floatval($_POST['boy'])), FILTER_SANITIZE_NUMBER_FLOAT);
+    $kilo = filter_var(htmlentities(floatval($_POST['kilo'])), FILTER_SANITIZE_NUMBER_FLOAT);
+    $sex = isset($_POST['sex']) ? $_POST['sex'] : '';
+
+    if ($boy && $kilo && $sex) {
+        calculate($boy, $kilo, $sex);
+    } else {
+        echo "Lütfen tüm alanları doldurun.";
+    }
+
+}
 
 ?>
 </div>
-    </div>
-    </div>
   </div>
 </body>
 </html>
